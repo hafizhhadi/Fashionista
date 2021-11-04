0893ccaf36ba459f60005213798e6a7630cdea13
Hafiz <hafizhadi581@gmail.com>
2021-11-04 23:30:22 +0800

    create details migration / added column image, address, phone number

---
+++ database/migrations/2021_11_04_132110_create_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image');  
+            $table->string('address');
+            $table->integer('phone_number'); //create table details / add column image, address , phone number
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details');
+    }
+}
