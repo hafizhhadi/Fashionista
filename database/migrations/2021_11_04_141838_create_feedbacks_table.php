29b6312aba720598a9809c2a954273ad4a68ebbb
Hafiz <hafizhadi581@gmail.com>
2021-11-04 23:32:55 +0800

    create migration feedbacks/ add column description, rate_id/ add foreign key for rate_id

---
+++ database/migrations/2021_11_04_141838_create_feedbacks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('rate_id')->nullable();
+            $table->string('description');
+            $table->timestamps();
+
+            $table->foreign('rate_id')->references('id')->on('ratings')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+}
