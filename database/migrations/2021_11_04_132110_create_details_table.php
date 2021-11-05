8867840210621f5ce86aae3a995aabf30cb78a32
Hafiz <hafizhadi581@gmail.com>
2021-11-05 20:22:33 +0800

    add foreign key user in details migration

--- database/migrations/2021_11_04_132110_create_details_table.php
+++ database/migrations/2021_11_04_132110_create_details_table.php
@@ -15,10 +15,13 @@ class CreateDetailsTable extends Migration
     {
         Schema::create('details', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id');
             $table->string('image');  
             $table->string('address');
             $table->integer('phone_number'); //create table details / add column image, address , phone number
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
