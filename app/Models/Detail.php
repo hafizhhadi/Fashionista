f46e0ddc7aba50691df6d19fefe0812c684bbd69
Hafiz <hafizhadi581@gmail.com>
2021-11-05 20:16:32 +0800

    add  relation model in detail table

--- app/Models/Detail.php
+++ app/Models/Detail.php
@@ -10,8 +10,14 @@ class Detail extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
         'image',
         'address',
         'phone_number',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
