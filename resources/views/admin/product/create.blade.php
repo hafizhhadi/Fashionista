43ff7778d10c465ce04b613cc03034fcdc1d48b7
Hafiz <hafizhadi581@gmail.com>
2021-11-07 10:42:30 +0800

    fix header name

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Add product') }}</div>
+                <div class="card-header">{{ __('Edit product') }}</div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('product:store') }}" enctype="multipart/form-data">
                         @csrf
