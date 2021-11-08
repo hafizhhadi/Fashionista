f4f68ef6e79066f0cb5f3c9d40bcd5385fd96638
Hafiz <hafizhadi581@gmail.com>
2021-11-08 19:45:27 +0800

    add nullable to image,address and phone number

--- database/migrations/2021_11_04_132110_create_details_table.php
+++ database/migrations/2021_11_04_132110_create_details_table.php
@@ -16,10 +16,10 @@ class CreateDetailsTable extends Migration
         Schema::create('details', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id');
-            $table->string('image');  
-            $table->string('address');
-            $table->integer('phone_number'); //create table details / add column image, address , phone number
-            $table->timestamps();
+            $table->string('image')->nullable();  
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable(); //create table details / add column image, address , phone number
+            $table->timestamps();   
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
