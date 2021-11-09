b7f8a104cf178a925998cbd2ca478cbfb027d4ca
Hafiz <hafizhadi581@gmail.com>
2021-11-09 16:48:07 +0800

    updated home page for user / fetch data to user

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -2,22 +2,39 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="row">
+        <div class="col">
+            <section class="py-5">
+                <div class="container px-4 px-lg-5 mt-5">
+                    <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                        @foreach ($products as $product )
+                        <div class="col mb-5">
+                            <div class="card h-100">
+                                <!-- Product image-->
+                                @if($product->image)
+                                    <img class="card-img-top" src="{{ asset('storage/'.$product->image) }}" alt="..." />
+                                @else
+                                    <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                                @endif
+                                <!-- Product details-->
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        <!-- Product name-->
+                                        <h5 class="fw-bolder">{{ $product->name }}</h5>
+                                        <!-- Product price-->
+                                        {{ $product->price }}
+                                    </div>
+                                </div>
+                                <!-- Product actions-->
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        @endforeach
+                    </div>
                 </div>
-            </div>
-        </div>
+            </section>
     </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -28,15 +28,18 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/admin/home', [HomeController::class, 'admin'])->name('admin:home')->middleware('is.admin');
 
 //user
+Route::get('/user/home/{user}', [UserController::class, 'index'])->name('user:index');
 Route::get('/user/profile/{user}', [UserController::class, 'show'])->name('user:show');
 Route::get('/user/profile/edit/{user}', [UserController::class, 'edit'])->name('user:edit');
 Route::post('/user/profile/update/{user}', [UserController::class, 'update'])->name('user:update');
 
-//product
+//product Admin
 Route::get('/product/index', [ProductController::class, 'index'])->name('product:index');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product:create');
 Route::post('/product/store', [ProductController::class, 'store'])->name('product:store');
 Route::get('/product/edit/{product}', [ProductController::class, 'edit'])->name('product:edit');
 Route::post('/product/update/{product}', [ProductController::class, 'update'])->name('product:update');
 Route::get('/product/delete/{product}', [ProductController::class, 'destroy'])->name('product:destroy');
+
+
 
